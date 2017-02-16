b4b7724994f99fb874efa53882f48fc66c38dd84
yonas samuel <yonastlolo@yahoo.com>
2017-02-15 21:04:31 -0500

    Switch-assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 
+$(function() {
 
+  $('.switch').click(function(){
+
+    $('button').toggleClass('on off');
+    $('body').toggleClass('light dark');
+
+    if( $('h1.status').text() === "It's so bright in here!" ){
+      $('h1.status').text("it's dark here");
+
+    } else{
+      $('h1.status').text("It's so bright in here!");
+    }
+  });
+
+});
